39f9674376566e56a209be5bacf37408750d87f4
Jesus Politron <polipop.work@gmail.com>
2022-10-12 18:13:29 -0500

    fixed API request, addded favorite cats print

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search";
+const API_URL_RANDOM =
+	"https://api.thecatapi.com/v1/images/search??api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp";
 
 const API_URL_FAVORITES =
 	"https://api.thecatapi.com/v1/favourites?api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp";
 
+// -------------------------------------------------- Load new cat
 async function reload() {
 	const res = await fetch(API_URL_RANDOM);
 	const data = await res.json();
@@ -17,11 +19,31 @@ async function reload() {
 	}
 }
 
-reload();
-
+// -------------------------------------------------- Save new cat
 async function favorites() {
-	const res = await fetch(API_URL);
+	const res = await fetch(API_URL_FAVORITES, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			image_id: "dje",
+		}),
+	});
+
 	const data = await res.json();
 
+	// console.log(res);
+}
+
+// -------------------------------------------------- Show saved cats
+async function load() {
+	const res = await fetch(API_URL_FAVORITES);
+	const data = await res.json();
+
+	console.log("Favoritos");
 	console.log(data);
 }
+
+reload();
+load();
