237764b8811efd6df71fd9fa62b5835a4675b276
Jesus Politron <polipop.work@gmail.com>
2022-10-12 14:48:15 -0500

    added error message for bad request, styles cleanup

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
-const API_URL =
-	"https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp";
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search";
+
+const API_URL_FAVORITES =
+	"https://api.thecatapi.com/v1/favourites?api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp";
 
 async function reload() {
+	const res = await fetch(API_URL_RANDOM);
+	const data = await res.json();
+
+	// console.log(data);
+
+	if (res.status !== 200) {
+		alert("Something is wrong: " + res.status + ", " + data.message);
+	} else {
+		const img = document.getElementById("catI");
+		img.src = data[0].url;
+	}
+}
+
+reload();
+
+async function favorites() {
 	const res = await fetch(API_URL);
 	const data = await res.json();
 
 	console.log(data);
-
-	const img = document.getElementById("catI");
-	img.src = data[0].url;
 }
-
-reload();
