ea04112799639c2f58370f37ab997299beeae818
Jesus Politron <polipop.work@gmail.com>
2022-10-13 14:52:39 -0500

    added delete and add actions to the michis project, cleanup some scripts for better legibility

--- js/index.js
+++ js/index.js
@@ -4,8 +4,11 @@ const API_URL_RANDOM =
 const API_URL_FAVORITES =
 	"https://api.thecatapi.com/v1/favourites?api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp";
 
-// -------------------------------------------------- Load new cat
-async function reload() {
+const API_URL_FAVORITES_DELETE = (id) =>
+	`https://api.thecatapi.com/v1/favourites/${id}?api_key=live_SJRWUOWTVzkAH2ucWp5vyDdj9qiHF6WgnSBTRWH3ecjvHJRSzEF3PnqsChqlL5Jp`;
+
+// -------------------------------------------------- Load a new cat
+async function newCat() {
 	const res = await fetch(API_URL_RANDOM);
 	const data = await res.json();
 
@@ -16,34 +19,83 @@ async function reload() {
 	} else {
 		const img = document.getElementById("catI");
 		img.src = data[0].url;
+
+		const btnAdd = document.getElementById("saveCat");
+		btnAdd.onclick = () => newFavoriteCat(data[0].id);
 	}
 }
 
-// -------------------------------------------------- Save new cat
-async function favorites() {
+// -------------------------------------------------- Save a new cat
+async function newFavoriteCat(id) {
 	const res = await fetch(API_URL_FAVORITES, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			image_id: "dje",
+			image_id: id,
 		}),
 	});
 
 	const data = await res.json();
 
 	// console.log(res);
+
+	if (res.status !== 200) {
+		alert("Something is wrong: " + res.status + ", " + data.message);
+	} else {
+		loadFavoriteCats();
+	}
 }
 
-// -------------------------------------------------- Show saved cats
-async function load() {
+// -------------------------------------------------- Load favorite cats
+async function loadFavoriteCats() {
 	const res = await fetch(API_URL_FAVORITES);
 	const data = await res.json();
 
-	console.log("Favoritos");
-	console.log(data);
+	// console.log("Favoritos");
+	// console.log(data);
+
+	if (res.status !== 200) {
+		alert("Something is wrong: " + res.status + ", " + data.message);
+	} else {
+		const section = document.getElementById("favorites");
+		section.innerHTML = "";
+
+		data.forEach((michi) => {
+			const article = document.createElement("article");
+			const img = document.createElement("img");
+			const button = document.createElement("button");
+			const btnText = document.createTextNode("Remove from favorites");
+
+			button.appendChild(btnText);
+			button.classList.add("deleteCat");
+			button.onclick = () => deleteFavorite(michi.id);
+			img.src = michi.image.url;
+
+			article.appendChild(img);
+			article.appendChild(button);
+
+			section.appendChild(article);
+		});
+	}
 }
 
-reload();
-load();
+// -------------------------------------------------- Delete a favorite cat
+
+async function deleteFavorite(id) {
+	const res = await fetch(API_URL_FAVORITES_DELETE(id), {
+		method: "DELETE",
+	});
+
+	const data = res.json();
+
+	if (res.status !== 200) {
+		alert("Something is wrong: " + res.status + ", " + data.message);
+	} else {
+		loadFavoriteCats();
+	}
+}
+
+newCat();
+loadFavoriteCats();
